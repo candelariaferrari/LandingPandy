b1c6ceeb072e1fae9cb6d2a570fbaf53101b390e
Cande Ferrari <cande@gmail.com>
2021-02-28 21:35:49 -0300

    primera subida

---
+++ js/data.js
@@ -0,0 +1,20 @@
+let datosPacks = [{
+    "id": 1,
+    "nombre": "Kasper",
+    "precio":10000,
+    encarrito: false,
+    cantidad: 0
+    },{
+    "id": 2,
+    "nombre": "Moro",
+    "precio":15000,
+    encarrito: false,
+    cantidad: 0
+    },{
+      "id": 3,
+      "nombre": "Pandy",
+      "precio":20000,
+      encarrito: false,
+      cantidad: 0
+    }
+];
---
+++ js/main.js
@@ -0,0 +1,109 @@
+/* header */ 
+/* crear boton */ 
+const botonPrueba = document.getElementById('cboton');
+const boton = document.createElement('button');
+
+botonPrueba.appendChild(boton);
+
+boton.textContent = 'Empezamos?'
+
+ /* boton conocenos */ 
+ $(document).ready(function(){ 
+  $('button').on('click',function(){
+     $('#startForm').toggle();
+  });
+});
+
+let dataFormulario= [];
+
+let  formulario = document.getElementById("formularioEmpezamos");
+formulario.onsubmit = (event) => {
+    event.preventDefault (); // evita el comportamiento por defecto del form de "volver a cargar la pag"
+    // para enviar la info del formulario 
+    let dataFormulario = {
+        nombre: event.target.children[0].value,
+    }
+    let padre = document.getElementById("notificacion"); // salida 
+    padre.innerHTML = `Gracias ${dataFormulario.nombre}, estamos listos para empezar!`;
+    console.log(dataFormulario); // para ver la info del formulario 
+    localStorage.setItem("nombre", dataFormulario.nombre);
+}
+
+
+
+
+/* PACKS */ 
+class productoPack {
+    constructor(datosPacks) {
+        this.id = datosPacks.id;
+        this.nombre = datosPacks.nombre;
+        this.precio = parseFloat(datosPacks.precio);
+    }
+}
+let carritoPack = []; // para la funcion carrito
+let padrePack = document.getElementById("contenedorPacks"); // contenedor de pack
+let carritopackHtml = document.getElementById("packsCargados"); // contendor de futura lista creada del carrito
+// funcion para crear un elemento del dom 
+function crearElementoPack() {
+    datosPacks.forEach(pack => { // para crear cada pack 
+        let nodo = document.createElement('div')
+        nodo.innerHTML =    `<div class="cpack">
+                                <h3 class="nombreP"> ${pack.nombre}</h3>
+                                <h4 class="precioP"> $${pack.precio}</h4>
+                            </div>
+                                <button id="${pack.nombre}" onclick="addToCart(${datosPacks.indexOf(pack)})" class="btnAgregarP"> Agregar al carrito </button>
+                            `;                        // para saber que index es
+        padrePack.appendChild(nodo);                       
+    })
+}
+crearElementoPack();
+
+function addToCart(index) { // funcion para el push carrito y sumar la cantidad 
+    let pack = datosPacks[index];
+    if (carritoPack.length > 0) {
+        let noExiste = true;
+        for (let i = 0; i < carritoPack.length; i++) { //si no se agrega
+            if (pack.nombre === carritoPack[i].nombre) {
+                carritoPack[i].cantidad++;
+                noExiste = false;
+            }
+        }
+        if (noExiste) { //si se agrega (.push)
+            pack.cantidad = 1;
+            carritoPack.push(pack);
+        }
+    } else {
+        pack.cantidad = 1;
+        carritoPack.push(pack)
+    }
+    renderCarritoPack();
+}
+//FUNCION RENDERIZAR CARRITO (GRAFICAR EL ARRAY)
+function renderCarritoPack() {
+    carritopackHtml.innerHTML = '';
+    if (carritoPack.length > 0) {
+        carritoPack.forEach(pack => {
+            carritopackHtml.innerHTML += `
+            <div>
+            <h3 class="nombreP"> ${pack.nombre}</h3>
+            <h4 class="precioP">${pack.precio * pack.cantidad}</h4>
+            <h5>${pack.cantidad}</h5>
+            <button id="${pack.nombre}" class="btnAgregarP" type="button" onclick='borradoDePack(${carritoPack.indexOf(pack)})'>Elimnar producto</button>
+            </div>
+            `
+        })
+    }
+}
+// PARA BORRAR (borra de a uno y cuando llega a cero lo quita de la lista)
+function borradoDePack(index) {
+    carritoPack[index].cantidad = carritoPack[index].cantidad - 1;
+    if (carritoPack[index].cantidad <= 0) {
+        carritoPack.splice(index, 1)
+    }
+    renderCarritoPack();
+}
+
+
+  
+
+
